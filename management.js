417046c5aed3061497cdfb05c4a65d6a4b277f57
Anthony Parrino <anthony21parrino21@gmail.com>
2020-11-22 13:41:41 -0500

    added first quetion and based upon answer run certain function

--- management.js
+++ management.js
@@ -3,23 +3,53 @@ const inquirer = require("inquirer");
 
 // created the connection information for the sql database
 const connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Database port
-  port: 3306,
+    // Database port
+    port: 3306,
 
-  // Databade username
-  user: "root",
+    // Databade username
+    user: "root",
 
-  // Database password and name
-  password: "password",
-  database: "management"
+    // Database password and name
+    password: "password",
+    database: "management"
 });
 
 // Database connection
-connection.connect(function(err) {
-  if (err) throw err;
-// call function here
+connection.connect(function (err) {
+    if (err) throw err;
+    questions();
 });
 
 // create function here
+function questions() {
+    // prompt for main question to move onto next set of questions
+    inquirer
+        .prompt([
+            {
+                name: 'main',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ["Add new employee", "Update an employee's role", "Terminate employee", "View all employees", "View all departments", "View all roles"]
+            }
+        ])
+        // based upon users first answer run the function for that response
+        .then(function(answer) {
+                if (answer.main === "Add new employee") {
+                    addEmployee();
+                } else if (answer.main === "Update an employee's role") {
+                    updateEmployee();
+                } else if (answer.main === "View all employees") {
+                    viewAllEmployees();
+                } else if (answer.main === "View all departments") {
+                    viewAllDepartments();
+                } else if (answer.main === "View all roles") {
+                    viewAllRoles();
+                } else if (answer.main === "Terminate employee") {
+                    fired();
+                } else {
+                    connection.end();
+                }
+        });
+}
